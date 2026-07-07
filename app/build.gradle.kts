e3b07117283a56238c7c58e3160623c6c94e917b
John Doe <calls-blocker@ryosoftware>
2026-07-07 11:58:18 +0200

    Increases app version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         minSdk = 30
         targetSdk = 37
 
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         buildConfigField(
             "String",
